3059e6cedd0b7a0b09784aae38df8fae5a4af15b
davidemessi <mdavide374@gmail.com>
2026-05-26 21:59:05 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors    = require('cors');
 const fs      = require('fs');
 const path    = require('path');
-const bcrypt  = require('bcrypt');
+const bcrypt  = require('bcryptjs');
 
 const multer = require('multer');
 
